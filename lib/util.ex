802f129b2492e1a75c02b82c9bbf5d9398be982c
Phil Toland <phil.toland@gmail.com>
2016-07-27 13:15:33 -0500

    Remove unused util module

--- lib/util.ex
+++
@@ -1,8 +0,0 @@
-defmodule MixErlangTasks.Util do
-  def filter_opts(opts) do
-    Enum.reduce(opts, [], fn
-      {name, nil}, acc -> [name|acc]
-      {name, val}, acc -> [name, val | acc]
-    end)
-  end
-end
